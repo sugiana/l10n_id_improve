881c75f88872267b10605b4d757079f84dc73548
Owo Sugiana <sugiana@gmail.com>
2026-08-16 06:12:43 +0700

    Currency format without fractional parts

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
 def pre_init(env):
-    query = """UPDATE res_lang SET date_format = '%A %d %b %Y'
-        WHERE iso_code = 'id'"""
-    env.cr.execute(query)
     query = """
         UPDATE ir_ui_view
         SET arch_db = jsonb_strip_nulls(jsonb_set(arch_db, '{id_ID}', 'null'))
--- __manifest__.py
+++ __manifest__.py
@@ -4,11 +4,15 @@
     'author': 'Owo Sugiana',
     'license': 'LGPL-3',
     'summary': 'Indonesian translation improvements and default localization',
-    'description': 'An example of the improvement is that the translation of State in the Contacts menu should be Province, not Status.',
+    'description': (
+        'An example of the improvement is that the translation of State in '
+        'the Contacts menu should be Province, not Status.'),
     'category': 'Localizations',
     'pre_init_hook': 'pre_init',
     'data': [
-        'data/res_company_data.xml',
-        'data/res_partner_data.xml',
+        'data/res_company.xml',
+        'data/res_lang.xml',
+        'data/res_currency.xml',
+        'data/ir_default.xml',
     ],
 }
